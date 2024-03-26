b06b56b167c5b50fd4fd06b56d86cdfeb3783944
RqtHqck <rathack13@gmail.com>
2024-03-27 02:17:33 +0300

    COMMIT. Finalized the function of passing a word to the user, added the ability to cancel and receive another word

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from aiogram import executor
-
+import asyncio
 from loader import dp
 import middlewares, filters, handlers
 from utils.set_bot_commands import set_default_commands
@@ -13,4 +13,5 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    loop = asyncio.get_event_loop()
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=dp.storage.close(), loop=loop, skip_updates=True)
--- handlers/users/learn_word.py
+++ handlers/users/learn_word.py
@@ -1,14 +1,18 @@
 from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 from funcs.db_work.db_process import readDbWords
 from data import config
+from keyboards.inline.nextWord import nextWord
+from aiogram.types import CallbackQuery
 import logging
+from funcs.db_work.takeWord import takeWord
 
+
+# Получаем слово из бд
 
 
 @dp.message_handler(commands=['learn_word'])
-async def bot_start(message: types.Message):
+async def learn_words(message: types.Message):
     """
     Эта функция нужна для учения* слов.
     Принцип: 
@@ -18,16 +22,29 @@ async def bot_start(message: types.Message):
     # -----Вызывается функция для того, чтобы взять новое слово из бд-----
 
     # Тут получаем рандомную строку 
-    
-    field_tuple = readDbWords(config.DB_NAME_WORDS)
-    if field_tuple:
-        logging.info(f'New word update {field_tuple}')
-        rus = field_tuple[1]
-        bel = field_tuple[2]
-    if field_tuple == False:
-         await message.answer(text=f'Вы вывучылі ўжо ўсе словы. Віншуем вас з гэтым. Вы прарабілі вялікую працу. Калі вы жадаеце дадаць новыя словы ў нашага бота, пішыце сюды:\n@RqtHqck')
+    translation = takeWord()
+    if translation:
+        logging.info(f'New word update {translation}')
+    else:
+        await message.answer(text=f'Вы вывучылі ўжо ўсе словы. Віншуем вас з гэтым. Вы прарабілі вялікую працу. Калі вы жадаеце дадаць новыя словы ў нашага бота, пішыце сюды:\n@RqtHqck')
+
+    await message.answer(text=f'Добра, вось ваша новае слова:\n {translation[0]} - {translation[1]}', reply_markup=nextWord())
 
 
+@dp.callback_query_handler(text='next')
+async def btn_next(callback: CallbackQuery):
+    await callback.answer(cache_time=60)
+    translation = takeWord()
+    if translation:
+        logging.info(f'New word update {translation}')
+    else:
+        await callback.message.answer(text=f'Вы вывучылі ўжо ўсе словы. Віншуем вас з гэтым. Вы прарабілі вялікую працу. Калі вы жадаеце дадаць новыя словы ў нашага бота, пішыце сюды:\n@RqtHqck')
 
-    await message.answer(text=f'Добра, вось ваша новае слова:\n {bel} - {rus}')
-    
+    await callback.message.answer(text=f'Добра, вось ваша новае слова:\n {translation[0]} - {translation[1]}', reply_markup=nextWord())
+
+@dp.callback_query_handler(text='cancel')
+async def btn_cancel(callback: CallbackQuery):
+    await callback.answer(cache_time=60)
+    await callback.message.delete()
+    await callback.message.answer(text="Вы выйшлі. Выбярыце каманду для бота: /help")
+    logging.info(msg=f"Exit from words learning")
